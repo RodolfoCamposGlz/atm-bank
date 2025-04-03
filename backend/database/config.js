71039d2fd9e7e6fdfc776ba478a7f35395a635f0
RodolfoCamposGlz <rodolfocampos459@gmail.com>
2025-04-02 18:25:37 -0600

    fixed docker database seed

--- backend/database/config.js
+++ backend/database/config.js
@@ -2,7 +2,7 @@ const { Op, Sequelize, QueryTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./database.sqlite3",
+  storage: "./database.sqlite",
 });
 
 module.exports = {
